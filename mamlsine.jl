fb57b9762e514a91bf5379ce710099bc50605eef
Alex Robson <alex.robson@invenialabs.co.uk>
2018-11-05 21:10:21 +0000

    Sorting plotting script

--- mamlsine.jl
+++ mamlsine.jl
@@ -7,7 +7,7 @@ using Random
 
 # Create a model class to explore
 P = 1
-f(t, A, ϕ) = A .* sin.(P*2*π*t .+ ϕ)
+f(t, A, ϕ) = A .* sin.(P*t .+ ϕ)
 
 function initialise()
   m = Chain(
@@ -25,72 +25,86 @@ function sample(N)
   map(p -> t -> f(t, p[1], p[2]), p_train)
 end
 
-function metatrain!(model, N, opt1, opt2, loss1)
+function metatrain!(model, N::Int, α::Float64, β::Float64, K::Int)
   for i in range(1, N)
     println("Epoch: $i")
     # Sample some tasks
-    tasks = sample(10)
-    θ_i = deepcopy(Flux.params(model))
-    _model = deepcopy(model)
-    metaloss = []
+    tasks = sample(25);
+    metaloss = [];
+    θ = Flux.params(model)
+
+    model_i = deepcopy(model);
+
+    metaopt = Flux.Optimise.ADAM(θ, β)
     for (t, task) in enumerate(tasks)
-      θ = deepcopy(θ_i) # Reinitialize the parameters
-      model = deepcopy(_model)
+
+      # Disconnect the model
+      θ_i = deepcopy(θ)
+      model_i = deepcopy(model)
+
+      batchopt = Flux.Optimise.ADAM(θ_i, α)
+
       # Sample K data points
       x_s = rand(Uniform(-5, 5), K)
-      data = [(map(x -> [x], x_s), task(x_s))]
+      data = [(map(x -> [x], x_s), task(x_s))];
+      loss1(x, y) = mean(Flux.mse.(model_i.(x), y))
       l = loss1(data[1]...)
-      #Flux.train!(loss1, data, opt1)
-      Flux.Tracker.back!(loss1(data[1]...)) # Backpropogate through metaloss
-      opt1() # Update
-
+      Flux.Tracker.back!(loss1(data[1]...)); # Backpropagate through batchloss
+      batchopt(); # Update
 
       # Resample
       x_s = rand(Uniform(-5, 5), K)
-      data = [(map(x -> [x], x_s), task(x_s))]
-
-      # Evaluate loss at these new parameters
-      l = loss1(data[1]...)
-      push!(metaloss, l)
+      data = [(map(x -> [x], x_s), task(x_s))];
+      l = loss1(data[1]...) # Evaluate loss at these new params
+      push!(metaloss, l);
     end
-    model = deepcopy(_model)
-    opt = Flux.Optimise.runall(opt2)
+
+    metaopt = Flux.Optimise.runall(metaopt)
     l = sum(metaloss)
     Flux.Tracker.back!(l) # Backpropogate through metaloss
-    opt() # Update
+    metaopt() # Update
   end
 end
 
 
-K = 10
-α = 1e-2
-β = 1e-2
 model, θ = initialise()
 loss1(x, y) = mean(Flux.mse.(model.(x), y))
-opt_1 = Flux.Optimise.ADAM(θ, α)
-opt_2 = Flux.Optimise.ADAM(θ, β)
 
-metatrain!(model, 1000, opt_1, opt_2, loss1)
-
+metatrain!(model, 50000, 1e-3, 1e-3, 10)
+#model = deepcopy(model_metatrained)
 # Attempt K-shot learning
+#model = deepcopy(model_metatrained)
 tasks = sample(5)
 task = tasks[1]
 
 ### Before update
 x_test = -5:0.01:5
 y_test = task.(x_test)
-y_pred = map(x -> x[1].data, model.(map(x-> [x], x_test)))
-scatter(x_test, y_test)
-scatter!(x_test, y_pred)
+y_pred_0 = map(x -> x[1].data, model.(map(x-> [x], x_test)))
+
 
 ### Update
 
+# Store model before updating
+model_metatrained = deepcopy(model)
+
 x_s = rand(Uniform(-5, 5), K)
 data = [(map(x -> [x], x_s), task(x_s))]
-Flux.train!(loss1, data, opt1)
+metaopt = Flux.Optimise.ADAM(Flux.params(model), 1e-3)
+@epochs 1 Flux.train!(loss1, data, metaopt)
 x_test = -5:0.01:5
 y_test = task.(x_test)
-y_pred = map(x -> x[1].data, model.(map(x-> [x], x_test)))
+y_pred_1 = map(x -> x[1].data, model.(map(x-> [x], x_test)))
 
-scatter(x_test, y_test)
-scatter!(x_test, y_pred)
+
+@epochs 9 Flux.train!(loss1, data, metaopt)
+y_pred_10 = map(x -> x[1].data, model.(map(x-> [x], x_test)))
+
+p = plot(x_test, y_test)
+p = plot!(x_test, y_pred_0)
+p = plot!(x_test, y_pred_1)
+p = plot!(x_test, y_pred_10)
+p = scatter!(x_s, task(x_s))
+
+
+plot(p, label = ["True", "Before Update", "1 Update", "10 Updates", "Sampled Data"])
